80ccbe835888f17b5a13abcd6a87d54662561b41
KienNguyen9 <kierannguyen98@gmail.com>
2023-09-13 16:09:52 +0700

    Update file VD chuong 1

---
+++ C1_khai_niem_co_ban/Vi_du/Vd_1_chuong_trin_C/Vd_1_chuong_trinh_C.c
@@ -0,0 +1,12 @@
+/*Chuong trinh in ra 2 dong chu*/
+#include "stdio.h" /*Thu vien vao ra chuan*/
+#include "conio.h"
+void main() /*Ham chinh*/
+{
+    int clrscr(); /*xoa man hinh chinh*/
+    /*Xuong dong (\n) va IN trong lap trinh C*/
+    printf("\n C programming language");
+    /*Xuong dong va in lam quen voi ban*/
+    printf("\n Nice to meet you.");
+    getch(); /*Tam dung may de xe ket qua*/
+}
---
+++ C1_khai_niem_co_ban/Vi_du/Vi_du_chuong_trinh_C_2/main.c
@@ -0,0 +1,10 @@
+#include "stdio.h"
+#include "conio.h"
+
+void main()
+{
+    /*Xuong dong, in ra dong chu "VScode han hanh"
+    lai xuong dong va in "lam quen voi ban" */
+    printf("\nVSCODE HAN HANH \ 
+    \nLAM QUEN VOI BAN");   
+}
