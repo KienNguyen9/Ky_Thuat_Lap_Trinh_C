9e0e73b24db34a1615f4ec5e9aab5923e4ead7f3
KienNguyen9 <kierannguyen98@gmail.com>
2023-09-18 15:48:04 +0700

    Update VD enum

--- C2_Hang_bien_mang/Vidu_3_Bien_kieu_enum/main.c
+++ C2_Hang_bien_mang/Vidu_3_Bien_kieu_enum/main.c
@@ -1,11 +1,16 @@
 /*Kiểu dữ liệu enum
+cách dùng: 
+    enum tên_kiểu_cần_tạo {phần_tử_1, pt2,...} tên_biến 
 */
 #include "stdio.h"
 int main()
+
 {   
-    enum {T0, T1, T2};
+    /*Kiểu enum này không có tên kiểu, chỉ có phần tử*/
+    enum {T0, T1, T2}; 
+    /*Kiểu enum này là kiểu các thứ trong tuần, tên kiểu là "day", n1 là tên biến được tạo*/
     enum day {cn, t2, t3, t4, t5, t6,t7} n1;
-    enum day n2;
+    enum day n2; // n2 là tên biến được tạo
     int i,j =2000, k = T2;
     i = t7;
     n1 = -1000;
