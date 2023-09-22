0e2a530c7fb55758166795461ed57bad378fb40f
KienNguyen9 <kierannguyen98@gmail.com>
2023-09-23 01:20:31 +0700

    Upload Vi du minh hoa

---
+++ C3_Bieu_Thuc/Test_chuong_trinh/main.c
@@ -0,0 +1,7 @@
+#include "stdio.h"
+int main()
+{
+    int a = 11;
+    printf("%x",0xa1b6&0xff);
+    return 0;
+}
---
+++ C3_Bieu_Thuc/Vidu_1_Minh_hoa_chuong_trinh_sai/main.c
@@ -0,0 +1,32 @@
+/*Chương trình sau là sai vì không tuân thủ đúng các nguyên tắc sử dụng
+    Chương trình minh họa
+    - việc chuyển kiểu trong biểu thức
+    - sự thực hiện của các phép toàn logic
+    - cách sử dụng các phép toán tăng giảm
+*/
+#include "stdio.h"
+#include "conio.h" //
+int main()
+{
+    int clrscr();
+    printf("\n 1.5 * (11/3) = %0.2f\n 1.5*11/3 = %0.2f", 1.5*(11/3),1.5*11/3);
+    printf("\n (11/3) * 1.5 = %0.2f\n 11/3*1.5 = %0.2f", (11/3)*1.5,11/3*1.5);
+    printf("\n 11 / 3 + 1.5 = %0.2f", 11/3 + 1.5);
+    printf("\n Gia tri cua quan he 3 > 7 la: %d",3>7);
+    printf("\n Gia tri cua quan he 7 > 3 la: %d",7>3);
+    printf("\n Gia tri cua phep toan 3 && 7 la: %d", 3&&7);
+    {
+        int x = 10;
+        int y = 11;
+        /* Không thận trọng khi dùng phép tăng giảm 
+        -> KẾT QUẢ SAI*/
+        printf("\n Khi x = 10 va y = 11 thi cau lenh: ");
+        printf("\n printf(\"\\n\%6)");
+        printf("d\\n\%6");
+        printf("d\\n\%6");
+        printf("d\", se in ra (gia tri sai):");
+        printf("\n %6d\n%6d\n%6d", x-- * ++y, x-- - --y,x++ + ++y);
+    }
+    getch();
+    return 0;
+}
