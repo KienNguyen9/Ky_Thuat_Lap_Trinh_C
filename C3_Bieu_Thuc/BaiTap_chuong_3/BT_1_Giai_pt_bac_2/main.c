3cd7767084c31fbdca09008c8cb7374e71f1fef9
KienNguyen9 <kierannguyen98@gmail.com>
2023-09-27 15:15:20 +0700

    Update BT chuong 3

--- C3_Bieu_Thuc/BaiTap_chuong_3/BT_1_Giai_pt_bac_2/main.c
+++ C3_Bieu_Thuc/BaiTap_chuong_3/BT_1_Giai_pt_bac_2/main.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <math.h>
-
+/*
 // Hàm giải phương trình bậc 2
 int giaiPT(double a, double b, double c, double *x1, double *x2) {
   // Tính delta
@@ -45,5 +45,46 @@ int main() {
     printf("Phương trình có hai nghiệm x1 = %lf và x2 = %lf.\n", x1, x2);
   }
 
+  return 0;
+}
+*/
+
+// Hàm tính PT bậc 2
+  int GiaiPT(double a, double b, double c, double *x1, double *x2)
+  {
+    double delta = b*b - 4*a*c;
+    if(delta < 0){
+      //PT vô nghiệm
+      return 0;
+    } else if(delta ==0){
+      //PT có nghiệm kép
+      *x1 = *x2 = -b / (2*a);
+      return 1;
+    } if (delta > 0 )
+    {
+      //Pt có 2 nghiệm
+      *x1 = (-b + sqrt(delta)) / (2 * a);
+      *x2 = (-b - sqrt(delta)) / (2 * a); 
+      return 2;
+    }
+  }
+// Hàm main
+int main(){
+  //Nhập liệu
+  double a,b,c;
+  printf("Hay nhap cac he so cua PT:");
+  scanf("%lf%lf%lf",&a,&b,&c);
+  // Tạo 2 biến nghiệm
+  double x1,x2;
+
+  int n = GiaiPT(a,b,c,&x1,&x2);
+  //In KQ
+  if(n == 0){
+    printf("\nPT vo nghiem");
+  } else if(n == 1){
+    printf("\nx1 = x2 = %lf",x1);
+  }else{
+    printf("\nx1 = %lf\nx2 =%lf",x1,x2);
+  }  
   return 0;
 }
