4350d0421a66791b3ac17ca4eddb68ed03ee8b7c
KienNguyen9 <kierannguyen98@gmail.com>
2023-10-18 23:26:05 +0700

    Update files

--- C5_Cac_toan_tu_dieu_khien/VD_7_FOR_dao_nguoc_day_so/main.c
+++ C5_Cac_toan_tu_dieu_khien/VD_7_FOR_dao_nguoc_day_so/main.c
@@ -1,9 +1,23 @@
-/*VD 1: 
-*/
-#include"stdio.h"
+#include "stdio.h"
 #include "conio.h"
-float 
+//Cho dãy số và tìm số phần tử
+float x[] = {1.2,3.4,5.6,7.8,9};
+int n =  sizeof(x)/sizeof(float);
 int main()
 {
+    int i,j;
+    float c;
+    for(i=0,j=n-1; i<j; ++i,--j)
+    {
+        c = x[i];
+        x[i] = x[j];
+        x[j] = c;
+    }
+    
+    for(int z = 0; z<n; z++)
+    {
+        printf("%.2f - ",x[z]);
+    }
+   
     return 0;
 }
