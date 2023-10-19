5db74a08c92e54b6af268b92f16c6cb181701b21
KienNguyen9 <kierannguyen98@gmail.com>
2023-10-19 21:08:36 +0700

    Update files

--- C5_Cac_toan_tu_dieu_khien/VD_7_FOR_dao_nguoc_day_so/main.c
+++ C5_Cac_toan_tu_dieu_khien/VD_7_FOR_dao_nguoc_day_so/main.c
@@ -3,6 +3,7 @@
 //Cho dãy số và tìm số phần tử
 float x[] = {1.2,3.4,5.6,7.8,9};
 int n =  sizeof(x)/sizeof(float);
+/*
 int main()
 {
     int i,j;
@@ -19,5 +20,19 @@ int main()
         printf("%.2f - ",x[z]);
     }
    
+    return 0;
+}
+*/
+
+// CÁCH 2
+int main()
+{
+    int i,j;
+    float c;
+    for(i = 0, j = n-1; i<j; c = x[i], x[i] = x[j], x[j] = c, i++, j--)
+    {}
+    for(int z = 0; z<n ; z++){
+        printf("  %0.2f   ",x[z]);
+    }
     return 0;
 }
