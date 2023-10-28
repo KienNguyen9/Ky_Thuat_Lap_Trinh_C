4e88a705b2a80c1f9d154c229fcae7bd032adeb6
KienNguyen9 <kierannguyen98@gmail.com>
2023-10-28 15:24:30 +0700

    Update files

---
+++ C5_Cac_toan_tu_dieu_khien/BT_03_Lap_PT_tinh_ex_theo_CT/main.c
@@ -0,0 +1,18 @@
+/**
+ * @file main.c
+ * @author Kien Nguyen  
+ * @brief Lap PT tính ex theo cong thức
+ * 
+ * @version 0.1
+ * @date 2023-10-28
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+#include"stdio.h"
+#include"math.h"
+int main()
+{
+    // Bỏ qua 
+    return 0;
+}
